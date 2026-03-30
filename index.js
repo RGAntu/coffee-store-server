504bc7213091446a8b7f447e48358435c74b3b24
RGAntu <rgantu887@gmail.com>
2026-03-30 12:57:47 +0600

    app.post coffees

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
 const app = express();
 const port = 3000;
 
@@ -7,9 +10,48 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Coffee Server");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m3lhrmy.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollections = client.db("coffee-store").collection("coffee_collection");
+
+    app.get("/", (req, res) => {
+      res.send("Welcome to the Coffee Server");
+    });
+
+    app.post("/coffees", async(req, res) => {
+      const doc = req.body;
+      console.log(doc)
+      const result = await coffeeCollections.insertOne(doc);
+      res.send(result);
+
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Coffee server is running on port ${port}`);
