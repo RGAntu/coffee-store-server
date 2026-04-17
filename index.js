a2a6ca4af22027b21989bb82fa52ff40e11b7f26
RGAntu <rgantu887@gmail.com>
2026-04-17 22:36:00 +0600

    get single api id

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
        const allCoffees = await coffeeCollections.find().toArray();
       res.send(allCoffees)
     })
+    
+    app.get("/coffees/:id", async (req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollections.findOne(query);
+      res.send(result);
+    })
 
     app.post("/coffees", async(req, res) => {
       const doc = req.body;
